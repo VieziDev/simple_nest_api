dd3756326a2a2136e2c63450af239d34e57e5226
VieziDev <matheusviezi@gmail.com>
2025-04-18 14:55:45 -0300

    feat: Generate Tarefas resource module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TarefasModule } from './tarefas/tarefas.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TarefasModule],
+  imports: [TarefasModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
